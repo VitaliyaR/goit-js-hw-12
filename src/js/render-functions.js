c9eaf555af9253d488fce1ea7eebbb761672df43
Vitaliia_Rylach <vita.elmura@gmai.com>
2025-05-24 18:48:44 +0200

    add refresh

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,57 +1,63 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+const galleryContainer = document.querySelector('.gallery');
 
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 export const createGallery = (images) => {
-    const galleryContainer = document.querySelector('.gallery');
-  
-    const markup = images
-      .map(image => {
-        return `
-          <li class="gallery-item">
-            <a href="${image.largeImageURL}" class="gallery-link">
-              <img src="${image.webformatURL}" alt="${image.tags}" class="gallery-image" />
-            </a>
-            <div class="image-info">
-              <div class="info-labels">
-                <span>Likes</span>
-                <span>Views</span>
-                <span>Comments</span>
-                <span>Downloads</span>
-              </div>
-              <div class="info-values">
-                <span>${image.likes}</span>
-                <span>${image.views}</span>
-                <span>${image.comments}</span>
-                <span>${image.downloads}</span>
-              </div>
+  const markup = images
+    .map(image => {
+      return `
+        <li class="gallery-item">
+          <a href="${image.largeImageURL}" class="gallery-link">
+            <img src="${image.webformatURL}" alt="${image.tags}" class="gallery-image" />
+          </a>
+          <div class="image-info">
+            <div class="info-labels">
+              <span>Likes</span>
+              <span>Views</span>
+              <span>Comments</span>
+              <span>Downloads</span>
             </div>
-          </li>
-        `;
-      })
-      .join('');
+            <div class="info-values">
+              <span>${image.likes}</span>
+              <span>${image.views}</span>
+              <span>${image.comments}</span>
+              <span>${image.downloads}</span>
+            </div>
+          </div>
+        </li>
+      `;
+    })
+    .join('');
+
+  galleryContainer.insertAdjacentHTML('beforeend', markup);
+
   
-    galleryContainer.insertAdjacentHTML('beforeend', markup);
-  };
-  
-  export const clearGallery = () => {
-    document.querySelector('.gallery').innerHTML = '';
-  };
-  
-  export const showLoader = () => {
-    document.getElementById('loader').style.display = 'block';
-  };
-  
-  export const hideLoader = () => {
-    document.getElementById('loader').style.display = 'none';
-  };
-  
-  export const showLoadMoreButton = () => {
-    document.getElementById('load-more').style.display = 'inline-block';
-  };
-  
-  export const hideLoadMoreButton = () => {
-    document.getElementById('load-more').style.display = 'none';
-  };
-  
+  lightbox.refresh();
+};
+
+export const clearGallery = () => {
+  galleryContainer.innerHTML = '';
+};
+
+export const showLoader = () => {
+  document.getElementById('loader').style.display = 'block';
+};
+
+export const hideLoader = () => {
+  document.getElementById('loader').style.display = 'none';
+};
+
+export const showLoadMoreButton = () => {
+  document.getElementById('load-more').style.display = 'inline-block';
+};
+
+export const hideLoadMoreButton = () => {
+  document.getElementById('load-more').style.display = 'none';
+};
